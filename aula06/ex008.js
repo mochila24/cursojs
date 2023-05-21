26e46ad43255c6cb03a7ba608a9a4d852a85c7e0
Brunito <126920514+mochila24@users.noreply.github.com>
2023-05-21 02:31:35 -0300

    if multado

--- aula06/ex008.js
+++ aula06/ex008.js
@@ -1,2 +1,7 @@
-var vel = 60.5
+var vel = 60
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+// apertar f8 para rodar. 
