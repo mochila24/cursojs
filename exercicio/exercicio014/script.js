cae1da9e9facd93d1d4a3a8bb179cea56cfe042a
Brunito <126920514+mochila24@users.noreply.github.com>
2023-05-21 16:17:08 -0300

    exercício de hora do dia

--- exercicio/exercicio014/script.js
+++ exercicio/exercicio014/script.js
@@ -1,1 +1,20 @@
-alert(`Olá, Mundo!`)
+function carregar(){
+    var msg = window.document.querySelector('div#msg')
+    var img = window.document.getElementById('imagem')
+    var data = new  Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora}horas.`
+    if (hora >= 0 && hora < 12) {
+        //Bom dia!
+        img.src = 'bomdia.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora < 18) {
+        //Boa tarde!
+        img.src = 'boatarde.png'
+        document.body.style.background = '#b9846f'
+    }else {
+        //Boa noite!
+        img.src = 'boanoite.png'
+        document.body.style.background = 'black'
+    }
+}
